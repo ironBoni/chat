3bbcdfe6aff37e81d4f19910958e3d65010fc8f4
Noam Cohen <noam60@gmail.com>
2022-04-05 13:31:51 +0300

    made sign in as first

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import Chat from './Componentone/Chat/Chat';
 
-
 import { Route,BrowserRouter,Routes } from 'react-router-dom';
+import SignInPage from './Componentone/SignInPage/SignInPage';
 
 
 
 function App() {
   return(<BrowserRouter>
     <Routes>
-    <Route path="/" element={<Chat/>} />
-  
+    <Route path="/" element={<SignInPage/>} />
+    <Route path="/chat" element={<Chat/>} />
+
     </Routes>
     </BrowserRouter>);
 }
